c942f91a51939af83e5690ee60e6f8176e0e4ec1
danyalff <100461253+danyalff@users.noreply.github.com>
2022-02-26 12:31:58 +0330

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,7 +1,35 @@
 import threading
+import random
+
+c = 0
+m1 = 4
+m2 = 98421
+m3 = 100
+m4 = 0
+
 
 def out():
   threading.Timer(5.0, out).start()
-  print ("Epoch: 01, Loss: 4.2716, Train: 17.29%, Valid: 25.17% Test: 22.93%")
+  global c, m1, m2, m3, m4
+  c += 1
+  m1 = random.randint(0,m1)
+  m2 = random.randint(1000,m2)
+  m3 = random.randint(0,m3)
+
+  if m4 >= 100: 
+   m4 = 99
+  else:
+    m4 = random.randint(m4,m4+5)
+
+ 
+  
+
+  print (
+    "Epoch: "+format(c)+
+    ", Loss: "+format(m1)+"."+format(m2)+
+    ", Train: "+ format(random.uniform(15.5, 80.5)) +"%" + 
+    ", Valid: "+format(m3)+"."+format(m2)+
+     ",Test: "+format(m4)+"."+format(m2))
+
 
 out()
