3275b245def094ac09ae7ef46d6330faa0abb4d0
danyalff <100461253+danyalff@users.noreply.github.com>
2022-02-26 11:29:26 +0330

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,6 +2,6 @@ import threading
 
 def out():
   threading.Timer(5.0, out).start()
-  print "Epoch: 01, Loss: 4.2716, Train: 17.29%, Valid: 25.17% Test: 22.93%"
+  print ("Epoch: 01, Loss: 4.2716, Train: 17.29%, Valid: 25.17% Test: 22.93%")
 
 out()
